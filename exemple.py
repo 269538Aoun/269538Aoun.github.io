87e9bc4cba1f85793874f03a236db72db1cd74be
aounou.el_anziz_ben_said <aounouelanziz@gmail.com>
2025-09-22 09:16:12 +0200

    Modification du CV

---
+++ exemple.py
@@ -0,0 +1,11 @@
+class PC:
+    def __init__(self,nb_pouces, taille_clavier):
+        self.nb_pouces = nb_pouces
+        self.taille_clavier = taille_clavier
+        self.ecran = True
+        
+        
+        
+lenovo = PC(12,5)
+
+print(lenovo.nb_pouces)
